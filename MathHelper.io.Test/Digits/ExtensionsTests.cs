8e3cbb246b7d525daa548d6eb1f59f6aa418754c
Mikus Miroslav <miroslav.mikus@outlook.com>
2018-04-05 20:13:41 +0200

    Improvement of Digits tests

--- MathHelper.io.Test/Digits/ExtensionsTests.cs
+++ MathHelper.io.Test/Digits/ExtensionsTests.cs
@@ -7,62 +7,73 @@ namespace MathHelper.io.Test
     [TestClass]
     public class ExtensionsTests
     {
-        [TestMethod]
-        public void TestIsOdd()
-        {
-            int x = 5;
-            bool isOdd = x.IsOdd();
-            Assert.IsTrue(isOdd);
-            Assert.IsFalse(16.IsOdd());
-            Assert.IsTrue(111113.IsOdd());
-        }
+        [DataTestMethod]
+        [DataRow(5)]
+        [DataRow(17)]
+        [DataRow(111113)]
+        [DataRow(3547)]
+        public void TestIsOdd(int input) => Assert.IsTrue(input.IsOdd());
 
-        [TestMethod]
-        public void TestIsEven()
-        {
-            int x = 6;
-            bool isEven = x.IsEven();
-            Assert.IsTrue(isEven);
-            Assert.IsTrue(16.IsEven());
-            Assert.IsFalse(111113.IsEven());
-        }
+        [DataTestMethod]
+        [DataRow(4)]
+        [DataRow(16)]
+        [DataRow(111114)]
+        [DataRow(3546)]
+        public void TestIsNotOdd(int input) => Assert.IsFalse(input.IsOdd());
 
-        [TestMethod]
-        public void TestIsPrime()
-        {
-            Assert.IsTrue(2.IsPrime());
-            Assert.IsTrue(11.IsPrime());
-            Assert.IsTrue(13.IsPrime());
-            Assert.IsTrue(97.IsPrime());
-            Assert.IsTrue(127.IsPrime());
-            Assert.IsFalse(16.IsPrime());
-        }
+        [DataTestMethod]
+        [DataRow(4)]
+        [DataRow(16)]
+        [DataRow(111114)]
+        [DataRow(3546)]
+        public void TestIsEven(int input) => Assert.IsTrue(input.IsEven());
 
-        [TestMethod]
-        public void TestAdd()
-        {
-            Assert.IsTrue(10 == 2.Add(8));
-            Assert.IsTrue(-8 == 2.Add(-10));
-        }
+        [DataTestMethod]
+        [DataRow(5)]
+        [DataRow(17)]
+        [DataRow(111113)]
+        [DataRow(3547)]
+        public void TestIsNotEven(int input) => Assert.IsFalse(input.IsEven());
+
+        [DataTestMethod]
+        [DataRow(2)]
+        [DataRow(11)]
+        [DataRow(13)]
+        [DataRow(97)]
+        [DataRow(127)]
+        public void TestIsPrime(int input) => Assert.IsTrue(input.IsPrime());
+
+
+        [DataTestMethod]
+        [DataRow(4)]
+        [DataRow(16)]
+        [DataRow(22)]
+        [DataRow(22222222)]
+        [DataRow(128)]
+        public void TestIsNotPrime(int input) => Assert.IsFalse(input.IsPrime());
+
+
+        [DataTestMethod]
+        [DataRow(2, 8, 10)]
+        [DataRow(-2, 10, 8)]
+        public void TestAdd(int value1, int value2, int result) => Assert.IsTrue(result == value1.Add(value2));
 
 
         [TestMethod]
         public void TestSum()
         {
-            List<int> sum20 = new List<int>(){ 2, 10, 8 };
+            List<int> sum20 = new List<int>() { 2, 10, 8 };
             Assert.IsTrue(20 == sum20.Sum());
 
             List<int> sumNot20 = new List<int>() { 2, 13, 8 };
             Assert.IsFalse(20 == sumNot20.Sum());
         }
 
-        [TestMethod]
-        public void TestSubstract()
-        {
-            Assert.IsTrue(4 == 8.Substract(4));
-            Assert.IsTrue(100 == 8.Substract(-92));
-            Assert.IsFalse(4 == 8.Substract(8));
-        }
+        [DataTestMethod]
+        [DataRow(8, 4, 4)]
+        [DataRow(8, -92, 100)]
+        [DataRow(8, 8, 0)]
+        public void TestSubstract(int value1, int value2, int result) => Assert.IsTrue(result == value1.Substract(value2));
 
         [TestMethod]
         public void TestAverage()
@@ -70,14 +81,13 @@ namespace MathHelper.io.Test
             Assert.IsTrue(10 == new List<int>() { 2, 18 }.Average());
             Assert.IsTrue(20 == new List<int>() { 20, 18, 22 }.Average());
             Assert.IsFalse(2 == new List<int>() { 20, 18, 22 }.Average());
-
         }
 
         [TestMethod]
         public void TestWeightedAverage()
         {
-            Assert.IsTrue(5.5 == new List<double> { 8,4}.WeightedAverage(new List<double> {1.5,2.5}));
-            Assert.IsFalse(5.5 == new List<double> { 8,4}.WeightedAverage(new List<double> {1.5,2.6}));
+            Assert.IsTrue(5.5 == new List<double> { 8, 4 }.WeightedAverage(new List<double> { 1.5, 2.5 }));
+            Assert.IsFalse(5.5 == new List<double> { 8, 4 }.WeightedAverage(new List<double> { 1.5, 2.6 }));
         }
     }
 }
